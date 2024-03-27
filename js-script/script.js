58f3c0377443953ec51426590e587c7c16714280
MatteoRuffa <cortese_17@protonmail.com>
2024-03-27 15:33:47 +0100

    add img

--- js-script/script.js
+++ js-script/script.js
@@ -4,13 +4,17 @@ const app = createApp({
     data() {
         return {
             message: 'Giuro ci sto provando!',
-            mrClass: 'mr-font1'
+            mrClass: 'mr-font1',
+            image: '',
+            altImage: 'angry programmer'
         }
     },
     methods: {
         changeFont(){
             this.mrClass = this.mrClass === 'mr-font1' ? 'mr-font2' : 'mr-font1';
+        },
+        generatorImg(){
+            this.image = './img/OIG1.jfif'
         }
-
     }
 }).mount('#app');
