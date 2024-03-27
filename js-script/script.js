b63d559c5b9d1829c0d19d04e7b43dd5f4f63282
MatteoRuffa <cortese_17@protonmail.com>
2024-03-27 15:10:55 +0100

    add some try

--- js-script/script.js
+++ js-script/script.js
@@ -4,7 +4,13 @@ const app = createApp({
     data() {
         return {
             message: 'Giuro ci sto provando!',
-            mrClass: 'mr-font'
+            mrClass: 'mr-font1'
         }
+    },
+    methods: {
+        changeFont(){
+            this.mrClass = this.mrClass === 'mr-font1' ? 'mr-font2' : 'mr-font1';
+        }
+
     }
 }).mount('#app');
